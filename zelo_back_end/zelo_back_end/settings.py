ad48958721f92fe60fc5f8881671d8206c9c3804
Burumbay <sanzharbr@gmail.com>
2020-09-09 01:03:52 +0600

    Change socket host

--- zelo_back_end/zelo_back_end/settings.py
+++ zelo_back_end/zelo_back_end/settings.py
@@ -166,7 +166,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
+            "hosts": [('95.82.117.157', 6379)],
         },
     },
 }
