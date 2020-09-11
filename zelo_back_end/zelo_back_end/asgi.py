3c1f583c2c20cc4c02b5e8af2cd80b6b6410cc60
Burumbay <sanzharbr@gmail.com>
2020-09-11 16:52:07 +0600

    Change asgi file configuration

--- zelo_back_end/zelo_back_end/asgi.py
+++ zelo_back_end/zelo_back_end/asgi.py
@@ -2,7 +2,7 @@ import os
 import django
 from channels.routing import get_default_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zelo_back_end.settings")
 
 django.setup()
 
