c5091b3045e2cbccd0e2befdd09368362f9b0550
Burumbay <sanzharbr@gmail.com>
2020-09-29 18:21:57 +0600

    Edit user email print

--- zelo_back_end/user_app/consumers.py
+++ zelo_back_end/user_app/consumers.py
@@ -5,7 +5,7 @@ from channels.generic.websocket import WebsocketConsumer
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.user = self.scope["user"]
-        print(user.email)
+        print(self.user.email)
         self.group_name = 'users'
 
         async_to_sync(self.channel_layer.group_add)(
