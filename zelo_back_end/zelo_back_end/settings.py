6613ec0d87b1d651b6bdf15d124cbaded923c943
Burumbay <sanzharbr@gmail.com>
2020-12-02 21:23:24 +0600

    Add dev allowed host

--- zelo_back_end/zelo_back_end/settings.py
+++ zelo_back_end/zelo_back_end/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = [
                 'zelodostavka.me',
                 'localhost',
                 '207.154.207.70',
+                '207.154.213.83',
                 '127.0.0.1']
 CORS_ORIGIN_ALLOW_ALL = True
 
